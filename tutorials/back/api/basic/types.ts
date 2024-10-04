ed7a606fdc5df23b7642aa85df66b8a299bc2c8d
coita <larisa.coita@student.vinci.be>
2024-10-04 09:41:31 +0200

    1.3

---
+++ tutorials/back/api/basic/types.ts
@@ -0,0 +1,23 @@
+interface Pizza {
+  id: number;
+  title: string;
+  content: string;
+}
+
+interface PizzaToUpdate {
+  title?: string;
+  content?: string;
+}
+
+interface Drink {
+  id: number;
+  title: string;
+  image: string;
+  volume: number;
+  price: number;
+}
+
+export type { Pizza, NewPizza, PizzaToUpdate, Drink };
+
+type NewPizza = Omit<Pizza, "id">;
+
